8240a771cabf64d95d674736227fe1d3da6b0571
POGULA SAI SRI VARSHA <115077878+Varshapogula@users.noreply.github.com>
2024-05-18 15:31:57 +0530

    Update Affected.js

--- Affected.js
+++ Affected.js
@@ -1,4 +1,4 @@
-// Affected.js
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -7,7 +7,7 @@ function Affected() {
   const [data, setData] = useState({}); // State for fetched data
 
   useEffect(() => {
-    // Fetch data when selectedCountry changes
+    
     if (selectedCountry) {
       axios.get(`https://disease.sh/v3/covid-19/countries/${selectedCountry}`)
         .then(response => {
@@ -23,12 +23,10 @@ function Affected() {
     <div>
       <h1>Affected</h1>
       <select onChange={(e) => setSelectedCountry(e.target.value)}>
-        {/* Populate dropdown with country options */}
-        <option value="">Select Country</option>
-        {/* You can fetch country list from another API or hardcode it */}
+        
       </select>
       <div>
-        {/* Render line chart using data */}
+     
       </div>
     </div>
   );
